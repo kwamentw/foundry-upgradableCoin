be6032e84007750d3764afd14e06a5d143e5a4af
KWAME <antwi1496@hotmail.com>
2023-12-19 04:07:39 -0600

    first commit

---
+++ src/BoxV1.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 internal number;
+
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function Initialize(address newOwner) public initializer{
+        __Ownable_init(newOwner);
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber()  external view returns (uint256) {
+        return number;
+    }
+
+    function version() public pure returns (uint256){
+        return 1;
+    }
+
+        function _authorizeUpgrade(address newImplementation) internal override {}
+
+
+}
